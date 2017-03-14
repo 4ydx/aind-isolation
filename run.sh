db67a353c63d18df6d9be7798b77cc43b86f37ae
Nathan Findley <nathan@4ydx.com>
2017-03-14 19:42:38 +0900

    Possibly trying to always use a move even when failure seems possible may be a better winning strategy.

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
+# First edit tournament.py so that a single run performs more competitions
+
 cp game_agent.py.more_moves game_agent.py
-for i in {0..9}; do
-	python tournament.py > log/more-moves-$i.log 2>&1
-done
+python tournament.py > log/more-moves.log 2>&1
 
 cp game_agent.py.center_distance game_agent.py
-for i in {0..9}; do
-	python tournament.py > log/center-distance-$i.log 2>&1
-done
+python tournament.py > log/center-distance.log 2>&1
 
 cp game_agent.py.position_distance game_agent.py
-for i in {0..9}; do
-	python tournament.py > log/position-distance-$i.log 2>&1
-done
+python tournament.py > log/position-distance.log 2>&1
