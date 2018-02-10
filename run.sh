9af5d51a8f01bcc440100c3678e062ac54cf7feb
4ydx <nathan@4ydx@com>
2018-02-10 11:15:47 +0900

    Python extensions.

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 # First edit tournament.py so that a single run performs more competitions
 
-cp game_agent.py.1 game_agent.py
+cp game_agent.py.1.py game_agent.py
 python tournament.py > log/1.log 2>&1
 
-cp game_agent.py.2 game_agent.py
+cp game_agent.py.2.py game_agent.py
 python tournament.py > log/2.log 2>&1
 
-cp game_agent.py.3 game_agent.py
+cp game_agent.py.3.py game_agent.py
 python tournament.py > log/3.log 2>&1
 
-cp game_agent.py.4 game_agent.py
+cp game_agent.py.4.py game_agent.py
 python tournament.py > log/4.log 2>&1
 
-cp game_agent.py.5 game_agent.py
+cp game_agent.py.5.py game_agent.py
 python tournament.py > log/5.log 2>&1
 
-cp game_agent.py.6 game_agent.py
+cp game_agent.py.6.py game_agent.py
 python tournament.py > log/6.log 2>&1
